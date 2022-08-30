e20354ba8fda4df545b24ee90a21a5352295b306
NotHoudaifa <houdaifacherifi618@gmail.com>
2022-08-30 22:31:14 +0100

    updated App.jsx with basename prop to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import "./styles/App.css";
+import './styles/App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
 
-
 export default function App() {
     const [cart, setCart] = useState([]);
     const addToCart = (id) => {
         setCart(cart.concat(id));
-    }
+    };
     const checkoutHandler = () => {
         setCart([]);
-    }
+    };
     return (
         <>
-            <BrowserRouter>
-                <NavBar cartItems={cart.length}/>
+            <BrowserRouter basename="/">
+                <NavBar cartItems={cart.length} />
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/cart" element={<Cart cart={cart} checkout={checkoutHandler}/>} />
+                    <Route
+                        path="/cart"
+                        element={
+                            <Cart cart={cart} checkout={checkoutHandler} />
+                        }
+                    />
                     <Route path="/products" element={<Products />} />
-                    <Route path="/products/:id" element={<Product clickHandler={addToCart}/>} />
+                    <Route
+                        path="/products/:id"
+                        element={<Product clickHandler={addToCart} />}
+                    />
                 </Routes>
             </BrowserRouter>
         </>
