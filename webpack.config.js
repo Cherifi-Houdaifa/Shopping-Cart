ab67ca66582a988fc1d56a332ac37304b5bd54ca
NotHoudaifa <houdaifacherifi618@gmail.com>
2022-08-30 23:24:39 +0100

    added DefinPlugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode: "production",
+    mode: 'production',
     entry: {
-        main: path.resolve(__dirname, "./src/index.js"),
+        main: path.resolve(__dirname, './src/index.js'),
     },
     output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
     },
     devtool: false,
     devServer: {
-        static: path.resolve(__dirname, "./dist"),
-        port: "3000",
+        static: path.resolve(__dirname, './dist'),
+        port: '3000',
         open: true,
         hot: true,
         historyApiFallback: true,
     },
     resolve: {
-        extensions: [".js", ".jsx", ".json"],
+        extensions: ['.js', '.jsx', '.json'],
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
+                type: 'asset/resource',
             },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader",
+                    loader: 'babel-loader',
                 },
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.resolve(__dirname, "public/index.html")
-        })
-    ]
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+        new webpack.DefinePlugin({
+            'process.env.PUBLIC_PATH': process.env.PUBLIC_PATH,
+        }),
+    ],
 };
